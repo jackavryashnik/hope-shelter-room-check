3cb90ee03fcf452825260b5f8c4490a8ae6e11b0
jackavryashnik <jackavryashnik@gmail.com>
2024-06-04 14:29:05 +0200

    beds

--- src/components/Bed/Bed.jsx
+++ src/components/Bed/Bed.jsx
@@ -1,7 +1,14 @@
+import css from './Bed.module.css';
+
 const Bed = () => {
   return (
-    <div>Bed</div>
-  )
-}
+    <div className={css.bedWrapper}>
+      <label className={css.bed}>
+      <input type="checkbox" className={css.check} />
+      <span className={css.checkmark}></span>
+    </label>
+    </div>
+  );
+};
 
-export default Bed
+export default Bed;
--- src/components/TwoFloorsBed/TwoFloorsBed.jsx
+++ src/components/TwoFloorsBed/TwoFloorsBed.jsx
@@ -1,7 +1,18 @@
+import css from './TwoFloorsBed.module.css';
+
 const TwoFloorsBed = () => {
   return (
-    <div>TwoFloorsBed</div>
-  )
-}
+    <div className={css.bedWrapper}>
+      <label className={css.lowerBed}>
+        <input type="checkbox" className={css.check} />
+        <span className={css.checkmark}></span>
+      </label>
+      <label className={css.upperBed}>
+        <input type="checkbox" className={css.check} />
+        <span className={css.checkmark}></span>
+      </label>
+    </div>
+  );
+};
 
-export default TwoFloorsBed
+export default TwoFloorsBed;
