af74f4d660b12dcc793a4039b2cd8d17167b59ed
jackavryashnik <jackavryashnik@gmail.com>
2024-06-04 14:42:52 +0200

    add props

--- src/components/Bed/Bed.jsx
+++ src/components/Bed/Bed.jsx
@@ -1,12 +1,14 @@
 import css from './Bed.module.css';
 
-const Bed = () => {
+const Bed = ({ check }) => {
+  const isChecked = check;
+
   return (
     <div className={css.bedWrapper}>
       <label className={css.bed}>
-      <input type="checkbox" className={css.check} />
-      <span className={css.checkmark}></span>
-    </label>
+        <input type="checkbox" className={css.check} checked={isChecked} />
+        <span className={css.checkmark}></span>
+      </label>
     </div>
   );
 };
--- src/components/TwoFloorsBed/TwoFloorsBed.jsx
+++ src/components/TwoFloorsBed/TwoFloorsBed.jsx
@@ -1,14 +1,17 @@
 import css from './TwoFloorsBed.module.css';
 
-const TwoFloorsBed = () => {
+const TwoFloorsBed = ({ checkUp, checkDwn }) => {
+  const isCheckedUp = checkUp;
+  const isCheckedDwn = checkDwn;
+
   return (
     <div className={css.bedWrapper}>
       <label className={css.lowerBed}>
-        <input type="checkbox" className={css.check} />
+        <input type="checkbox" className={css.check} checked={isCheckedDwn} />
         <span className={css.checkmark}></span>
       </label>
       <label className={css.upperBed}>
-        <input type="checkbox" className={css.check} />
+        <input type="checkbox" className={css.check} checked={isCheckedUp} />
         <span className={css.checkmark}></span>
       </label>
     </div>
