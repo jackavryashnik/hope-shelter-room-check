3e4d1251f230f28cab2234a648ae4272da278973
jackavryashnik <jackavryashnik@gmail.com>
2024-06-30 18:15:11 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import css from './Button.module.css';
 
 const Button = ({ children }) => {
   return (
-    <button className={css.btn} disabled>
+    <button className={css.btn}>
       <span className={css.text}>{children}</span>
     </button>
   );
