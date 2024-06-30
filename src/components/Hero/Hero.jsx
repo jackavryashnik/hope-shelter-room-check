75447763674f33a1e012f44192ae6c32c325c08b
jackavryashnik <jackavryashnik@gmail.com>
2024-06-30 18:35:45 +0200

    center btn

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,9 @@ export default function Hero() {
     <div className={css.hero}>
       <h1 className={css.heroTitle}>Hope foundation</h1>
       <p className={css.heroText}>Helping those who really need it</p>
-      <Button>Check</Button>
+      <div className={css.btnContainer}>
+        <Button className={css.btn}>Check</Button>
+      </div>
     </div>
   );
 }
