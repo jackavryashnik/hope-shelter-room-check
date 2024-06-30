026882fa31d85c4a1cd939f65f6519a178f5dfb7
jackavryashnik <jackavryashnik@gmail.com>
2024-06-30 22:28:22 +0200

    create auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,7 +1,26 @@
-const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
-}
+import { NavLink, Navigate, useParams } from 'react-router-dom';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import RegisterForm from '../../components/RegisterForm/RegisterForm';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import css from './AuthPage.module.css';
 
-export default AuthPage
+const AuthPage = ({ isLoggedIn }) => {
+  const { authType } = useParams();
+
+  if (authType !== 'login' && authType !== 'register') {
+    return <NotFoundPage />;
+  }
+
+  return (
+    <div className={css.authPageContainer}>
+      <div>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/register">Register</NavLink>
+      </div>
+      {isLoggedIn && <Navigate to="/" />}
+      {authType === 'login' ? <LoginForm /> : <RegisterForm />}
+    </div>
+  );
+};
+
+export default AuthPage;
