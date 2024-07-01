ff21af522e912780ee46169574540a2858977f37
jackavryashnik <jackavryashnik@gmail.com>
2024-07-01 16:12:19 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,6 @@ import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import Hero from '../../components/Hero/Hero';
 import Layout from '../../components/Layout/Layout';
-import Room15 from '../../components/rooms/Room15/Room15';
 import css from './HomePage.module.css';
 
 const HomePage = ({ isLoggedIn }) => {
@@ -11,7 +10,6 @@ const HomePage = ({ isLoggedIn }) => {
       <Header isLoggedIn={isLoggedIn} />
       <Layout className={css.layout}>
         <Hero />
-        <Room15/>
       </Layout>
       <Footer />
     </div>
