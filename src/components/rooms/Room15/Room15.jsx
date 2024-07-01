21dd0d816a07df122f0b043c7e91a1e3d43e9a36
jackavryashnik <jackavryashnik@gmail.com>
2024-07-01 16:11:52 +0200

    create room 15

--- src/components/rooms/Room15/Room15.jsx
+++ src/components/rooms/Room15/Room15.jsx
@@ -1,17 +1,20 @@
 import Bed from '../../Bed/Bed';
+import BedHorizon from '../../BedHorizon/BedHorizon';
 import TwoFloorsBed from '../../TwoFloorsBed/TwoFloorsBed';
+import TwoFloorsBedHorizon from '../../TwoFloorsBedHorizon/TwoFloorsBedHorizon';
 import css from './Room15.module.css';
 
 const Room15 = () => {
   return (
     <div className={css.roomContainer}>
-      <div className={css.leftSide}>
+      <div className={css.top}>
         <Bed className={css.bed1} />
-        <Bed className={css.bed2} rotate={true} />
-      </div>
-      <div className={css.rightSide}>
-        <TwoFloorsBed className={css.bed3} rotate={true} />
+        <TwoFloorsBedHorizon className={css.bed3} />
         <TwoFloorsBed className={css.bed4} />
+      </div>
+      <div className={css.roomNumber}>15</div>
+      <div className={css.bottom}>
+        <BedHorizon className={css.bed2} />
       </div>
     </div>
   );
