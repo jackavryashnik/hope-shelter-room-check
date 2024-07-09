e0fff4aed54dc8c90eade6ded2957f2c4d308930
jackavryashnik <jackavryashnik@gmail.com>
2024-07-09 20:24:56 +0200

    add redirect if user authorizated

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useParams } from 'react-router-dom';
+import { useNavigate, NavLink, useParams } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import RegisterForm from '../../components/RegisterForm/RegisterForm';
 import NotFoundPage from '../NotFoundPage/NotFoundPage';
@@ -6,8 +6,18 @@ import css from './AuthPage.module.css';
 import clsx from 'clsx';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import { useEffect } from 'react';
 
 const AuthPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/');
+    }
+  }, [navigate]);
+
   const { authType } = useParams();
 
   if (authType !== 'login' && authType !== 'register') {
