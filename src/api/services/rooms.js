fabd9bc1a1ef9191442972f3aeaee26ad55222fd
jackavryashnik <jackavryashnik@gmail.com>
2024-07-11 14:41:26 +0200

    change server link to deployed backend

--- src/api/services/rooms.js
+++ src/api/services/rooms.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io.connect('http://localhost:3000', {
+export const socket = io.connect('https://hope-shelter-api.onrender.com/api/stats', {
   path: '/socket.io',
 });
