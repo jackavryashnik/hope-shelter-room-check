ac2e3aff33d8b88211ead8a5cd09cefe9012ed87
jackavryashnik <jackavryashnik@gmail.com>
2024-07-12 13:07:52 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://hope-shelter-api.onrender.com/api';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   withCredentials: true,
