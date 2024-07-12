09569b0845105cea70f569ced67ed90bcff63221
jackavryashnik <jackavryashnik@gmail.com>
2024-07-12 15:02:07 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   withCredentials: true,
-  baseURL: BASE_URL + '/api',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
