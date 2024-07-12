5931fa5aebcc1246fa809f6d88b5830256c82ee3
jackavryashnik <jackavryashnik@gmail.com>
2024-07-12 14:46:40 +0200

    update front enviroments variables

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   withCredentials: true,
-  baseURL: BASE_URL,
+  baseURL: BASE_URL + '/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
--- src/api/services/rooms.js
+++ src/api/services/rooms.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io.connect("https://hope-shelter-api.onrender.com/", {
+export const socket = io.connect(import.meta.env.VITE_BASE_URL, {
   path: '/socket.io',
 });
